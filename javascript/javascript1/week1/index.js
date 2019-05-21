61f68073054fb78f2ce886ec0bd1a9046948c02e
abedaarabi <45884020+abedaarabi@users.noreply.github.com>
2019-05-21 13:00:41 +0200

    Add files via upload

---
+++ javascript/javascript1/week1/index.js
@@ -0,0 +1,39 @@
+// Age-ify (A future age calculator)
+
+let yearOfBirth = 1991;
+let yearFuture = 2027
+let futureYear = yearFuture - yearOfBirth;
+
+console.log(futureYear);
+console.log('I WILL BE IN THE ' + futureYear + 'YEARS OLD IN ' + yearFuture);
+
+//Goodboy-Oldboy (A dog age calculator)
+let dogYearOfBirth = 2018;
+let dogYearFuture  = 2027;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let ageOfDog =  ageInHumanYear ;
+let ageInHumanYear = dogYearFuture - dogYearOfBirth; 
+let shouldShowResultInDogYears = true;
+if (shouldShowResultInDogYears){
+    console.log(" Your dog will be " + ageOfDog +  " dog years old in " +  dogYearFuture)
+}
+else{
+    console.log( "Your dog will be " + dogYear * 7 +" dog years old in " + yearFuture  )
+}
+
+// Housey pricey (A house price estimator)
+//______________INputs values_____________
+let width = 8;
+let height = 10;
+let depth = 8;
+let volumeInMeters =  width * height * depth + 1000; //the volume with the garden 'total'
+let housePrice = volumeInMeters * 2.5 * 1000 + 100 * 300;
+
+console.log(housePrice );
+if
+    (housePrice <= 2500000);{
+
+    console.log("Peter should take his father with him next time :D")
+    }
+
+
