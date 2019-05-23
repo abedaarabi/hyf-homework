bc0b91d0d9944b82a4b02ebca10959f391dd21c9
abedaarabi <45884020+abedaarabi@users.noreply.github.com>
2019-05-23 10:32:04 +0200

    add last exercise

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -1,19 +1,20 @@
+
 // Age-ify (A future age calculator)
 
-let yearOfBirth = 1991;
-let yearFuture = 2027
-let futureYear = yearFuture - yearOfBirth;
+const yearOfBirth = 1991;
+const yearFuture = 2027
+const futureYear = yearFuture - yearOfBirth;
 
 console.log(futureYear);
 console.log('I WILL BE IN THE ' + futureYear + 'YEARS OLD IN ' + yearFuture);
 
 //Goodboy-Oldboy (A dog age calculator)
-let dogYearOfBirth = 2018;
-let dogYearFuture  = 2027;
-let dogYear = dogYearFuture - dogYearOfBirth;
-let ageOfDog =  ageInHumanYear ;
-let ageInHumanYear = dogYearFuture - dogYearOfBirth; 
-let shouldShowResultInDogYears = true;
+const dogYearOfBirth = 2018;
+const dogYearFuture  = 2027;
+const dogYear = dogYearFuture - dogYearOfBirth;
+const ageOfDog =  ageInHumanYear ;
+const ageInHumanYear = dogYearFuture - dogYearOfBirth; 
+const shouldShowResultInDogYears = true;
 if (shouldShowResultInDogYears){
     console.log(" Your dog will be " + ageOfDog +  " dog years old in " +  dogYearFuture)
 }
@@ -23,11 +24,11 @@ else{
 
 // Housey pricey (A house price estimator)
 //______________INputs values_____________
-let width = 8;
-let height = 10;
-let depth = 8;
-let volumeInMeters =  width * height * depth + 1000; //the volume with the garden 'total'
-let housePrice = volumeInMeters * 2.5 * 1000 + 100 * 300;
+const width = 8;
+const height = 10;
+const depth = 8;
+const volumeInMeters =  width * height * depth + 1000; //the volume with the garden 'total'
+const housePrice = volumeInMeters * 2.5 * 1000 + 100 * 300;
 
 console.log(housePrice );
 if
@@ -36,4 +37,8 @@ if
     console.log("Peter should take his father with him next time :D")
     }
 
-
+//Ez Namey (Startup name generator) Optional
+const firstWords = ['Serendipity ','Petrichor ','Supine ','Solitude ','Aurora ','Idyllic ','Clinomania ','Pluviophile ','Euphoria','Sequoia '];
+const secondWords = ['letter ','word ','that ','has ','letter ','vowels','redwood ','tree','especially ','especially '];
+const  startupName = firstWords[Math.floor(Math.random()* 10 + 0)] + secondWords[Math.floor(Math.random()* 10 + 0)];
+console.log("startup name is" + startupName + ",  it is " + startupName.length + " characters ");
