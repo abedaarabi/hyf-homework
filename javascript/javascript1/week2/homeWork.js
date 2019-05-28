42c189938e11d4c1b2e5b5545cc0f21447e6c3d7
abedaarabi <45884020+abedaarabi@users.noreply.github.com>
2019-05-28 11:30:10 +0200

    V1.1

---
+++ javascript/javascript1/week2/homeWork.js
@@ -0,0 +1,44 @@
+
+
+// function getFullname(firstname, surname){
+//     return firstname + surname;
+// }
+// console.log(getFullname('Abed ','Aarabi'))
+
+
+
+
+//Flight booking fullname function
+
+  const fullname1 = true ;  //Trying to use global variable 
+  const fullname2 = true ;  
+
+  function getFullname(){
+  }
+ if(fullname1 == true){
+     console.log('Abed Aarabi');
+ }
+ else{
+     console.log('NOT SURE IF MY COSE RIGHT');  
+ } 
+ if(fullname2 == true){
+     console.log('Lord Abed Aarabi :D')
+ }  
+ else{
+     console.log('NOT SURE IF MY COSE RIGHT')
+ }
+//Event application
+
+
+
+
+//Weather wear
+const clothesToWear = 17;
+function winterClothes(){
+if(clothesToWear > 18){
+    console.log('put your jacket on')
+    }
+else{
+    console.log('keep you clothe')
+}
+}
