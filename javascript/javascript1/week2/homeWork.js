726a4ea957ed3070e39331a9ca8f2dadd4663c2a
abedaarabi <45884020+abedaarabi@users.noreply.github.com>
2019-05-29 08:38:51 +0200

    Update homeWork.js

--- javascript/javascript1/week2/homeWork.js
+++ javascript/javascript1/week2/homeWork.js
@@ -1,44 +1,53 @@
 
+// //Flight booking fullname function v1.0
+// const fullname1 = true ;  //Trying to use global variable 
+//   const fullname2 = true ;  
 
-// function getFullname(firstname, surname){
-//     return firstname + surname;
-// }
-// console.log(getFullname('Abed ','Aarabi'))
-
-
-
-
-//Flight booking fullname function
-
-  const fullname1 = true ;  //Trying to use global variable 
-  const fullname2 = true ;  
-
-  function getFullname(){
-  }
- if(fullname1 == true){
-     console.log('Abed Aarabi');
- }
- else{
-     console.log('NOT SURE IF MY COSE RIGHT');  
- } 
- if(fullname2 == true){
-     console.log('Lord Abed Aarabi :D')
- }  
- else{
-     console.log('NOT SURE IF MY COSE RIGHT')
- }
-//Event application
+//   function getFullname(){
+//   }
+//  if(fullname1 == true){
+//      console.log('Abed Aarabi');
+//  }
+//  else{
+//      console.log('NOT SURE IF MY COSE RIGHT');  
+//  } 
+//  if(fullname2 == true){
+//      console.log('Lord Abed Aarabi :D')
+//  }  
+//  else{
+//      console.log('NOT SURE IF MY COSE RIGHT')
 
 
 
 
-//Weather wear
-const clothesToWear = 17;
-function winterClothes(){
-if(clothesToWear > 18){
-    console.log('put your jacket on')
+
+
+
+
+
+//Flight booking fullname function v1.1
+const firstName = 'Abd ';
+const surname = ' Aarabi';
+const formalName =" Lord ";
+let useFormalName = [1, 0];
+let fullName1;
+let fullName2;
+
+function getFullName(firstName,surName,useFormalName){
+    if(useFormalName === 1){
+        fullName1 = formalName + firstName + surName;
+        return fullName1;
     }
-else{
-    console.log('keep you clothe')
+    else{
+        fullName2 = firstName + surName;
+        return fullName2;
+    }
 }
-}
+
+console.log(getFullName(firstName, surname, useFormalName[0]));
+//[0] = Lord Abd  Aarabi
+//[1] =  Abd  Aarabi
+
+
+
+
