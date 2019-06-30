435b6b4f3822779569deba8a6914a65bf51e4526
abedaarabi <45884020+abedaarabi@users.noreply.github.com>
2019-06-30 12:26:05 +0200

    Add files via upload

---
+++ javascript/javascript2/functions.js
@@ -0,0 +1,40 @@
+// const arr = [
+//     function() {
+//         console.log('function 1');
+        
+//     },
+    
+//     function() {
+//         console.log('function 2');
+        
+//     },
+
+//     function fuc3(x)  {
+//         console.log('function 3' + x);
+        
+//     }
+// ]
+// arr.forEach(item =>  item(5))
+
+/**
+ * ------------------------------------------------------
+ */
+
+// function example1()  {
+//     console.log('Habibi' );
+    
+// }
+// const example2 = () =>  console.log('hello')
+// example1()
+// example2()
+
+/**
+ * ------------------------------------------------------
+ */
+const example = () => console.log('Hej');
+
+ const obj = {
+     name: example,
+ };
+
+obj.name()
