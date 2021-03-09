b9a5ff78d5e7c4d11070ae3891c0cf5d4a0f46a0
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2021-03-09 06:54:44 +0100

    form input

---
+++ react/week3/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Addtodo from "./Todo";
+function App() {
+  return (
+    <div className="App">
+      <Addtodo redLine="line" />
+    </div>
+  );
+}
+
+export default App;
